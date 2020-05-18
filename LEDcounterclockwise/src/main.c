6e1a342543e07780e9dc0cbc2eb857122d4a6a38
Amine El Omari <elomariamin@outlook.de>
2020-05-18 12:28:26 +0200

    Refactoring

--- LEDcounterclockwise/src/main.c
+++ LEDcounterclockwise/src/main.c
@@ -2,14 +2,14 @@
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
 //#include <stdio.h>
-/**
- * sfsdgfsdfsd
- */
+
 void msDelay(uint32_t msTime);
 GPIO_InitTypeDef GPIO_LEDgreen;
 GPIO_InitTypeDef GPIO_LEDred;
 GPIO_InitTypeDef GPIO_LEDorange;
 GPIO_InitTypeDef GPIO_LEDblue;
+GPIO_InitTypeDef GPIOA_Config;
+void EXTI0_IRQHandler();
 void msDelay(uint32_t msTime)
 {
 	for(uint32_t i=0; i<msTime*4000;i++);
@@ -19,7 +19,14 @@ int main(void)
 {
 
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-/////////////////////////////////////////////////
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+
+//  configuration for the User Button:
+  GPIOA_Config.GPIO_Mode = GPIO_Mode_IN;
+  GPIOA_Config.GPIO_Pin = GPIO_Pin_0;
+  GPIOA_Config.GPIO_PuPd = GPIO_PuPd_DOWN;
+  GPIO_Init(GPIOA, &GPIOA_Config);
 
 GPIO_LEDgreen.GPIO_Pin = GPIO_Pin_12;
 GPIO_LEDgreen.GPIO_Mode = GPIO_Mode_OUT;
@@ -43,6 +50,36 @@ GPIO_Init(GPIOD,&GPIO_LEDred);
 GPIO_Init(GPIOD,&GPIO_LEDblue);
 GPIO_Init(GPIOD,&GPIO_LEDorange);
 
+/* Enable clock for SYSCFG */
+	    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	    /* Tell system that you will use PA0 for EXTI_Line0 */
+	    	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+
+	    	EXTI_InitTypeDef EXTI_Config;
+	    	/* PA0 is connected to EXTI_Line0 */
+	    	EXTI_Config.EXTI_Line = EXTI_Line0;
+	    	/* Enable interrupt */
+	    	EXTI_Config.EXTI_LineCmd = ENABLE;
+	    	/* Interrupt mode */
+	    	EXTI_Config.EXTI_Mode = EXTI_Mode_Interrupt;
+	    	/* Triggers on rising and falling edge */
+	    	EXTI_Config.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	    	/* Add to EXTI */
+	    	EXTI_Init(&EXTI_Config);
+
+	// Configuration EXTI0 interrupt control:
+		NVIC_InitTypeDef NVICEXTI0_Config;
+
+		NVICEXTI0_Config.NVIC_IRQChannel = EXTI0_IRQn ;
+		NVICEXTI0_Config.NVIC_IRQChannelCmd = ENABLE;
+		NVICEXTI0_Config.NVIC_IRQChannelPreemptionPriority = 0x00;
+		NVICEXTI0_Config.NVIC_IRQChannelSubPriority = 0x00;
+		NVIC_Init(&NVICEXTI0_Config);
+
+		EXTI0_IRQHandler();
+
+
   /* Initialize LEDs */
  // STM_EVAL_LEDInit(LED3);
   //STM_EVAL_LEDInit(LED4);
@@ -60,6 +97,7 @@ GPIO_Init(GPIOD,&GPIO_LEDorange);
 //int k=1;
   while (1)
   {
+
 	  GPIO_WriteBit(GPIOD, GPIO_Pin_14, Bit_SET);
 	  msDelay(1000);
 	  GPIO_WriteBit(GPIOD, GPIO_Pin_14, Bit_RESET);
@@ -92,6 +130,20 @@ GPIO_Init(GPIOD,&GPIO_LEDorange);
 
   }
 }
+
+void EXTI0_IRQHandler(void){
+
+	if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+
+		/* Do your stuff when PA0 is PRESSED */
+			GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_SET);
+			msDelay(1000);
+			GPIO_WriteBit(GPIOD, GPIO_Pin_12, Bit_RESET);}
+			/* Clear interrupt flag */
+				EXTI_ClearITPendingBit(EXTI_Line0);
+
+
+		}
 
 
 /*
